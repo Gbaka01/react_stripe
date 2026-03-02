6bfaa748e3230e6f86d841fb89773b4adaa0891e
Gbaka01 <gore.goli@gmail.com>
2026-03-02 10:18:12 +0100

    commitgg234

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -10,7 +10,7 @@ Sa démarche actuelle se distingue par une réinterprétation minimaliste de th�
 
 
 
-Entre académisme et expression sensible, Goré Gbaka Goli cherche à capter la lumière pour en faire le vecteur principal des émotions. La simplicité du geste souligne ainsi la profondeur des scènes bibliques ou historiques, transformant chaque ligne en une quête de clarté et de ressenti.
+Entre académisme et expression sensible, Gore Gbaka Goli cherche à capter la lumière pour en faire le vecteur principal des émotions. La simplicité du geste souligne ainsi la profondeur des scènes bibliques ou historiques, transformant chaque ligne en une quête de clarté et de ressenti.
            </p> 
            <div className="card m-5 col-8">
            <iframe className="pdf" src="/certification_gore-gbaka-goli_559.pdf" title="PDF"></iframe>
